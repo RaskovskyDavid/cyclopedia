691e9ec8be442823370274c54aae8cfaadee8476
david <davidtucu@hotmail.com>
2023-12-21 15:10:57 -0300

    controlar componentes

--- src/CyclOPediaClassPage.jsx
+++ src/CyclOPediaClassPage.jsx
@@ -9,6 +9,8 @@ class CyclOPediaClassPage extends React.Component {
       studentList: [],
       studentCount: 0,
       hideInstructor: false,
+      inputName: "",
+      inputFeedback: "",
     };
   }
 
@@ -66,7 +68,28 @@ class CyclOPediaClassPage extends React.Component {
           </div>
 
         )}
-
+ <div className="p-3">
+          <span className="h4 text-success">Feedback</span>
+          <br />
+          <input
+            type="text"
+            value={this.state.inputName}
+            placeholder="Name.."
+            onChange={(e) => {
+              this.setState({ inputName: e.target.value });
+            }}
+          ></input>{" "}
+          Value : {this.state.inputName}
+          <br />
+          <textarea
+            value={this.state.inputFeedback}
+            onChange={(e) => {
+              this.setState({ inputFeedback: e.target.value });
+            }}
+            placeholder="Feedback..."
+          ></textarea>
+          Value : {this.state.inputFeedback}
+        </div>
 <div className="p-3">
           <span className="h4 text-success">Students</span> <br />
           <div>Student Count : {this.state.studentCount}</div>
