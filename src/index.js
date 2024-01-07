bcbcdae42faf70ab3a1e5dc41f44d65910edc3e2
david <davidtucu@hotmail.com>
2024-01-07 18:36:47 -0300

    Initial Setup - Functional Component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import CyclOPediaFuncPage from './CyclOPediaFuncPage';
 import Header from './Header';
 import CyclOPediaClassPage from './CyclOPediaClassPage';
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,6 +12,10 @@ root.render(
           <span className="h1 text-warning text-center">Class Component</span>
           <CyclOPediaClassPage />
         </div>
+        <div className="col-6">
+        <span className="h1 text-warning text-center">Func Component</span>
+        <CyclOPediaFuncPage />
+      </div>
       </div>
     </div>
   );
